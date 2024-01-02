443497000afbc11c96ee9629f07e798449dbfd17
Dan Pomeranz <dapomeranz@Dans-MacBook-Air.local>
2024-01-02 14:24:12 -0500

    lint

--- chocolate_factory/tests/check_yield_over_100.sql
+++ chocolate_factory/tests/check_yield_over_100.sql
@@ -1,6 +1,3 @@
 -- models/daily_yield_by_resource_test.sql
 -- Test to ensure that yield_rate is never greater than 100
-SELECT
-  *
-FROM {{ ref('daily_yield_by_resource') }}
-WHERE "yield_rate (%)" > 100
+select * from {{ ref("daily_yield_by_resource") }} where "yield_rate (%)" > 100
