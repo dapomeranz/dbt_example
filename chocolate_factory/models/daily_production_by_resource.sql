5d431215090a4a22f6d73c7bbcb8cddf3182f556
Dan Pomeranz <dapomeranz@Dans-MacBook-Air.local>
2024-01-02 17:30:36 -0500

    fixed daily production with uom conversion

--- chocolate_factory/models/daily_production_by_resource.sql
+++ chocolate_factory/models/daily_production_by_resource.sql
@@ -1,6 +1,9 @@
 select
     date_trunc('day', cl.timestamp) as date,
     cl.resource_id,
-    sum(cl.quantity) as quantity
+    sum(cl.quantity * coalesce(uom.conversion_rate, 1)) as quantity
 from {{ source("chocolate-factory", "mes_complete_logs") }} cl
+inner join
+    {{ source("chocolate-factory", "erp_work_orders") }} wo on cl.work_order_id = wo.id
+left join {{ ref("uom_conversions") }} uom on uom.product_id = wo.product_id
 group by 1, 2
